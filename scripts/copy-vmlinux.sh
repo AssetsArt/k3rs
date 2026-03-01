6b67491cf62b5fcd45b1ce6f6bac5a273d5abe79
detoro <meanstack20@gmail.com>
2026-03-01 17:26:11 +0700

    feat: Enable initrd build, update kernel artifact storage path, and default example to VM runtime.

--- scripts/copy-vmlinux.sh
+++ scripts/copy-vmlinux.sh
@@ -1,3 +1,3 @@
-mkdir -p /tmp/k3rs-runtime/kernel
-cp build/kernel/vmlinux /tmp/k3rs-runtime/kernel/vmlinux
-cp build/kernel/initrd.img /tmp/k3rs-runtime/kernel/initrd.img
+mkdir -p /tmp/k3rs-data/runtime/kernel
+cp build/kernel/vmlinux /tmp/k3rs-data/runtime/kernel/vmlinux
+cp build/kernel/initrd.img /tmp/k3rs-data/runtime/kernel/initrd.img
